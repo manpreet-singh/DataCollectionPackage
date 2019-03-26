16a4acb2fc58b17f812bc16c6b50d3a76ae6e2c8
Manpreet Singh <singh-manpreet@outlook.com>
2019-03-26 14:06:17 -0700

    Basic Setup & Test

---
+++ RocketMain.py
@@ -0,0 +1,8 @@
+import board
+import busio
+import adafruit_fxos8700
+import adafruit_fxas21002c
+
+i2c = busio.I2C(board.SCL, board.SDA)
+fxos = adafruit_fxos8700.FXOS8700(i2c)
+fxas = adafruit_fxas21002c.FXAS21002C(i2c)
