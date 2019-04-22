3c857ea579ac83ccfda5d1c9b811be7b2a179507
Manpreet Singh <singh-manpreet@outlook.com>
2019-04-22 12:46:09 -0700

    Added more data to output

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ echo "Transferring Project dir over SCP."
 scp BEMO.py pi@192.168.137.50:/home/pi/Documents/Workspace/
 ssh pi@192.168.137.50 "cd ~/Documents/Workspace/; sudo python BEMO.py"
 
-echo "Retrieving Data"
+echo "\nRetrieving Data"
 scp pi@192.168.137.50:/home/pi/Documents/Workspace/data.csv data.csv
